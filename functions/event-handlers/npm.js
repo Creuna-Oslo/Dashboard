c8c11c887ba3a638f2cbf3994147411ea4aedcd0
Asbjørn Hegdahl <asbjorn.hegdahl@creuna.no>
2018-08-01 00:32:16 +0200

    Adds initial npm event listener

---
+++ functions/event-handlers/npm.js
@@ -0,0 +1,21 @@
+const crypto = require("crypto");
+const functions = require("firebase-functions");
+
+module.exports = request => {
+  const signature = request.headers["x-npm-signature"];
+
+  // From: https://github.com/npm/npm-hook-receiver/blob/master/index.js#L24
+  const expected = crypto
+    .createHmac("sha256", functions.config().npm.secret)
+    .update(request._body)
+    .digest("hex");
+  if (signature !== "sha256=" + expected) {
+    console.log("Bad signature");
+  }
+
+  const { body } = request;
+  const { name } = body;
+  const version = body.payload["dist-tags"].latest;
+
+  return { name, version };
+};
--- functions/index.js
+++ functions/index.js
@@ -2,12 +2,14 @@ const admin = require("firebase-admin");
 const functions = require("firebase-functions");
 
 const gitHubEventHandlers = require("./event-handlers/github");
+const NPMEventHandler = require("./event-handlers/npm");
 const travisEventHandler = require("./event-handlers/travis");
 
 admin.initializeApp(functions.config().firebase);
 const database = admin.database();
 
 exports.onGitHubHook = functions.https.onRequest((request, response) => {
+  // TODO: Move all request handling to github.js
   const eventType = request.headers["x-github-event"];
   const eventHandler = gitHubEventHandlers[eventType];
   const notification = eventHandler && eventHandler(request.body);
@@ -34,10 +36,17 @@ exports.onGitHubHook = functions.https.onRequest((request, response) => {
 
 exports.onNPMHook = functions.https.onRequest((request, response) => {
   database.ref("debug").push(JSON.stringify(request.body));
+
+  const package = NPMEventHandler(request);
+  database
+    .ref("packages")
+    .child(package.name)
+    .update(package);
   response.status(200).send();
 });
 
 exports.onTravisHook = functions.https.onRequest((request, response) => {
+  // TODO: Move all request handling to travis.js
   const buildStatus = travisEventHandler(JSON.parse(request.body.payload));
 
   if (!buildStatus) {
