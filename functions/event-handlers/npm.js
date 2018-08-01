180f86008c2c3fca80a074c215dcaab8e72ca064
Asbjørn Hegdahl <asbjorn.hegdahl@creuna.no>
2018-08-01 09:44:28 +0200

    Adds some logs to npm event handler

--- functions/event-handlers/npm.js
+++ functions/event-handlers/npm.js
@@ -3,6 +3,8 @@ const functions = require("firebase-functions");
 
 module.exports = request => {
   const signature = request.headers["x-npm-signature"];
+  console.log(request._body);
+  console.log(Buffer.from(request._body));
 
   // From: https://github.com/npm/npm-hook-receiver/blob/master/index.js#L24
   const expected = crypto
