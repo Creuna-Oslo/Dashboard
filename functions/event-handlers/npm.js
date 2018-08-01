a123907a51c94118b8e6299d82aa6e5092e742f1
Asbjørn Hegdahl <asbjorn.hegdahl@creuna.no>
2018-08-01 10:30:43 +0200

    More random things

--- functions/event-handlers/npm.js
+++ functions/event-handlers/npm.js
@@ -3,13 +3,11 @@ const functions = require("firebase-functions");
 
 module.exports = request => {
   const signature = request.headers["x-npm-signature"];
-  // console.log(request._body);
-  console.log(Buffer.from(request.body));
 
   // From: https://github.com/npm/npm-hook-receiver/blob/master/index.js#L24
   const expected = crypto
     .createHmac("sha256", functions.config().npm.secret)
-    .update(Buffer.from(request.body))
+    .update(JSON.stringify(request.body))
     .digest("hex");
   if (signature !== "sha256=" + expected) {
     console.log("Bad signature");
