96d704f0dbc31a2f4af627ca2340ffa27151862f
Asbjørn Hegdahl <asbjorn.hegdahl@creuna.no>
2019-01-04 16:51:48 +0100

    Adds StatsPage

---
+++ app/source/static-site/pages/stats-page/stats-page.jsx
@@ -0,0 +1,17 @@
+/*
+name: Statistics
+path: /stats
+*/
+
+import React from 'react';
+
+import Layout from '../../layout';
+import Stats from 'components/stats';
+
+const StatsPage = () => (
+  <Layout>
+    <Stats />
+  </Layout>
+);
+
+export default StatsPage;
