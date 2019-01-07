70addccbaea709df1c8b127d5d585409498ea0b7
Pelle Bjerkestrand <pelle@example.com>
2019-01-07 09:54:20 +0100

    hides audio controls when muted.

--- app/source/components/audio-controls/audio-controls.jsx
+++ app/source/components/audio-controls/audio-controls.jsx
@@ -16,31 +16,24 @@ const AudioControls = ({
       <div className="audio-action">
         <IconButton icon={isMuted ? 'mute' : 'unmute'} onClick={onClickMute} />
       </div>
-      <div className="audio-action">
-        <IconButton
-          disabled={isMuted}
-          icon="triangle-right"
-          onClick={() => {
-            !isMuted && onClickPlay();
-          }}
-        />
-      </div>
-      <div className="audio-action audio-volume">
-        <Icon className="audio-volume-icon" name="volume" />
-        <input
-          className="audio-volume-input"
-          disabled={isMuted}
-          onChange={e => {
-            if (isMuted) {
-              return;
-            }
-
-            onChangeVolume(Number(e.target.value));
-          }}
-          type="range"
-          value={volume}
-        />
-      </div>
+      {isMuted ? null : (
+        <React.Fragment>
+          <div className="audio-action">
+            <IconButton icon="triangle-right" onClick={onClickPlay} />
+          </div>
+          <div className="audio-action audio-volume">
+            <Icon className="audio-volume-icon" name="volume" />
+            <input
+              className="audio-volume-input"
+              onChange={e => {
+                onChangeVolume(Number(e.target.value));
+              }}
+              type="range"
+              value={volume}
+            />
+          </div>
+        </React.Fragment>
+      )}
     </div>
   </div>
 );
